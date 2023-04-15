f5e1e1164b120061e267244bc1150a2d6f1f0422
Federico Barbieri <federicobarbieri54@gmail.com>
2023-04-15 11:56:48 +0200

    f

--- backend/functions/openai_requests.py
+++ backend/functions/openai_requests.py
@@ -24,7 +24,7 @@ def convert_audio_to_text(audio_file):
 def get_chat_response(message_input):
 
   messages = get_recent_messages()
-  user_message = {"role": "user", "content": message_input + " Answer back in english and with irony"}
+  user_message = {"role": "user", "content": message_input + " Answer back in english and with a friendly tone"}
   messages.append(user_message)
   print(messages)
 
